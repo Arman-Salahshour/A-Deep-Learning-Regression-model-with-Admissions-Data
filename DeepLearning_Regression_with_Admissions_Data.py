29211a357ae8b631d8b8dd14707d8596f85add2e
Arman Salahshour <salahshour_a@yahoo.com>
2022-08-17 12:12:49 +0430

    Add Important Libraries.

---
+++ DeepLearning_Regression_with_Admissions_Data.py
@@ -0,0 +1,25 @@
+"""Add Important Libraries"""
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+from itertools import product
+
+import tensorflow as tf
+from tensorflow	import keras
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.callbacks import EarlyStopping
+from tensorflow.keras import layers
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.wrappers.scikit_learn import KerasRegressor
+
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import RandomizedSearchCV
+from sklearn.model_selection import GridSearchCV
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import Normalizer
+from sklearn.dummy import DummyRegressor
+from sklearn.metrics import r2_score
+from sklearn.metrics import mean_absolute_error
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import make_scorer
